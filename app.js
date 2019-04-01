6582679acc6d70cd068a558a692a52d46c9dffd9
Nunny Reyes <nunnyreyes@gmail.com>
2019-04-01 00:23:11 -0400

    linked style sheet correctly

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
-let moves = 0;
+let moves1 = 0;
+let moves2 = 0;
 let  cat1 = "Luna"
-let cat2 = "Garfield"
+let cat2 = "Artemis"
 
 
 $(document).ready(function (count) {
     console.log("ready!");
     $('#luna').click(function () {
-        moves++
-        console.log(moves);
-        document.getElementById('counter').innerHTML = "You have clicked the cat " + moves + " time(s)" 
+        moves1++
+        console.log(moves1);
+        document.getElementById('counter1').innerHTML = "You have clicked " + cat1 + " " + moves1 + " time(s)" ;
+        
+
 
     });
     
+})
+
+$(document).ready(function (count) {
+    console.log("ready!");
+    $('#artemis').click(function () {
+        moves2++
+        console.log(moves2);
+        document.getElementById('counter2').innerHTML = "You have clicked " + cat2 + " " + moves2 + " time(s)";
+
+
+
+    });
+
 })
 
 
