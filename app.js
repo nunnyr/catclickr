f734f85391c3f49b46c3062603cef61b09537d09
Nunny Reyes <nunnyreyes@gmail.com>
2019-03-31 23:32:37 -0400

    Counter works
    git add .
    ^C
    ^D

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 let moves = 0;
 
-$(document).ready(function () {
+
+$(document).ready(function (count) {
     console.log("ready!");
     $('#luna').click(function () {
         moves++
         console.log(moves);
+        document.getElementById('counter').innerHTML = "You have clicked the cat " + moves + " time(s)" 
 
     });
+    
 })
+
 
