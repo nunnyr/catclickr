60a482ebc22fd239bc2ff35d6f4360f646d83d30
Nunny Reyes <nunnyreyes@gmail.com>
2019-04-02 17:19:19 -0400

    Edited counter output

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <h1>CatClicker</h1>
 
 <h2>Motivation</h2>
-This project was created during Udacity's frontend nanodegree program. The styling and content was up to the developer. I ended up choosing animated cats from my childhoor for a user to select and "feed" by clicking it.
+This project was created during Udacity's frontend nanodegree program. The styling and content was up to the developer. I ended up choosing animated cats from my childhood for a user to select and "feed" by clicking it.
 
 <h2>Build Status</h2>
 This project is currently being edited as of April 2019. 
@@ -14,6 +14,7 @@ Frameworks used include:
     <li>jQuery</li>
     <li>Boostrap</li>
 </ul>
+
 <h2>How to use?</h2>
 The name of the project is very self explanatory. I've chosen some of the cats that were highlights from my youth and listed them in the dropdown menu. Choose a cat, learn a fact about them and click to see a counter increment. 
 
--- app.js
+++ app.js
@@ -1,33 +1,36 @@
 let moves = 0;
 // let luna = document.getElementById('#luna');
-let artemis = document.getElementById('#artemis');
-
-let cat= document.getElementById('');
-let salem = "I am a creature from the planet Mau and live with my human Sailor Serena. You have clicked me "
+//let artemis = document.getElementById('#artemis');
 
 
 $(document).ready(function () {
     console.log("ready");
-    $('.form-group').click(function (event) {
+    // $('.form-group').click(function (event) {
+        $('.form-control').click(function(event) { 
         let clickTarget = event.target;
         console.log("i am the clickTarget:", clickTarget);
         let cat = $('#sel1 option:selected').text();
         console.log("I chose " + cat);
         document.getElementById('name').innerHTML = "Hello, I am " + cat + ". ";
         
+        })
     })
-});
+// });
 
 $(document).ready(function (count) {
     console.log("ready!");
     $('#luna').click(function () {
         moves++
         console.log(moves);
-        document.getElementById('count').innerHTML = "You have fed me " + moves + " time(s).";
+        document.getElementById('count').innerHTML = "You have fed me " + moves + " time.";
+        if(moves > 1) {
+            document.getElementById('count').innerHTML = "You have fed me " + moves + " times.";
+        }
         
 
 
     });
+
     
 })
 
