a363eb819cf320da901cb55794f90d09ed6c9308
Nunny Reyes <nunnyreyes@gmail.com>
2019-04-03 15:43:57 -0400

    Working on linking form with img to display when selected

--- app.js
+++ app.js
@@ -2,62 +2,57 @@ let moves = 0;
 let cat;
 
 
-
-
 $(document).ready(function () {
     console.log("ready");
     // $('.form-group').click(function (event) {
         $('.form-control').click(function(event) { 
-        setTimeout(function() {
-        let clickTarget = event.target;
-        console.log("i am the clickTarget:", clickTarget);
-        console.log("I chose " + cat);
-        greeting(cat);
-        
+            setTimeout(function() {
+            let clickTarget = event.target;
+            console.log("i am the clickTarget:", clickTarget);
+            greeting();
+            resetMoves()
+            
         }, 1000)
-
-        })
     })
-// });
+    
+})
 
-
-function greeting(hello) {
-    let cat = $('#sel1 option:selected').text();
-    document.getElementById('name').innerHTML = "Hello, I am " + cat + ". ";
-
+//displaying cat chosen
+function showImage() {
+    $(li).addClassList('show');
 }
 
 
+
+
+//greeting + feeding counter 
+
+function greeting(hello) {
+    let cat = $('#sel1 option:selected').text();
+    
+    document.getElementById('name').innerHTML = "Hello, I am " + cat + ". ";
+    console.log("I chose " + cat);
+   
+}
 
 
 $(document).ready(function (count) {
     console.log("ready!");
-    $('#luna').click(function () {
+    $('li').click(function () {
         moves++
         console.log(moves);
         document.getElementById('count').innerHTML = "You have fed me " + moves + " time.";
-        if(moves > 1) {
+        if(moves > 1 && moves === 0) {
             document.getElementById('count').innerHTML = "You have fed me " + moves + " times.";
-        }
+        }        
         
-
-
     });
-
-    
 })
 
-
-
-function showCat (cat) {
-    cat.removeClassList('hidden');
-    cat.addClassList('show');
+//choosing a different cat 
+function resetMoves() {
+    moves = 0;
+    console.log("I am resetting the moves counter")
+    
 }
-
-
-
-
-// when you click something
-// get that id
-// update the moves counter and say a sentence
 
